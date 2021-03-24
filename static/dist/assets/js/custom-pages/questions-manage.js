f515da4c84fcc3747bfdc9fb2861693f24fc94d6
Steve X <stevexyh@hotmail.com>
2021-03-24 19:38:17 +0800

    feat: added file upload MIME type restriction

--- static/dist/assets/js/custom-pages/questions-manage.js
+++ static/dist/assets/js/custom-pages/questions-manage.js
@@ -148,3 +148,17 @@ $(document).ready(function () {
         },
     });
 });
+
+Dropzone.options.fileDropzoneCSV = {
+    acceptedFiles: ".csv",
+    addRemoveLinks: true,
+    method: "post",
+    filesizeBase: 1024,
+    paramName: "file", // The name that will be used to transfer the file
+    maxFilesize: 5, // MB
+    dictResponseError: "上传失败",
+    dictRemoveFile: "删除文件",
+    dictCancelUpload: "dictCancelUpload",
+    dictFileTooBig: "文件大小: {{filesize}} MB\n最大限制: {{maxFilesize}} MB",
+}
+alert("Dropzone.JS加载正常\nRemove Before Flight");
