32bcc7fd1b917877d12bd4b84495f892802ea6c3
Steve X <stevexyh@hotmail.com>
2021-04-04 00:42:27 +0800

    chore(main): added headers for python

--- src/SQL_memOJi/urls.py
+++ src/SQL_memOJi/urls.py
@@ -1,4 +1,19 @@
-"""SQL_memOJi URL Configuration
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'''
+----------------------------------------------------------------------------------------------------
+* Project Name : SQL_memOJi
+* File Name    : urls.py
+* Description  : 
+* Create Time  : 2021-04-04 00:39:15
+* Version      : 1.0
+* Author       : Steve X
+* GitHub       : https://github.com/Steve-Xyh/SQL_memOJi
+----------------------------------------------------------------------------------------------------
+* Notice
+
+SQL_memOJi URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
@@ -12,7 +27,10 @@ Class-based views
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+----------------------------------------------------------------------------------------------------
+'''
+
+
 from django.contrib import admin
 from django.urls import path
 
--- src/SQL_memOJi/wsgi.py
+++ src/SQL_memOJi/wsgi.py
@@ -1,11 +1,27 @@
-"""
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'''
+----------------------------------------------------------------------------------------------------
+* Project Name : SQL_memOJi
+* File Name    : wsgi.py
+* Description  : 
+* Create Time  : 2021-04-04 00:40:14
+* Version      : 1.0
+* Author       : Steve X
+* GitHub       : https://github.com/Steve-Xyh/SQL_memOJi
+----------------------------------------------------------------------------------------------------
+* Notice
+
 WSGI config for SQL_memOJi project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
-"""
+----------------------------------------------------------------------------------------------------
+'''
+
 
 import os
 
