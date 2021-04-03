afa835f52b6c3ec6e3ab04fb87c1d2caa5896eb5
Steve X <stevexyh@hotmail.com>
2021-04-04 00:44:01 +0800

    chore(user): added headers for python

--- src/user/admin.py
+++ src/user/admin.py
@@ -1,3 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'''
+----------------------------------------------------------------------------------------------------
+* Project Name : SQL_memOJi
+* File Name    : admin.py
+* Description  : 
+* Create Time  : 2021-04-04 00:42:50
+* Version      : 1.0
+* Author       : Steve X
+* GitHub       : https://github.com/Steve-Xyh/SQL_memOJi
+----------------------------------------------------------------------------------------------------
+* Notice
+- 
+- 
+----------------------------------------------------------------------------------------------------
+'''
+
+
 from django.contrib import admin
 
 # Register your models here.
--- src/user/tests.py
+++ src/user/tests.py
@@ -1,3 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'''
+----------------------------------------------------------------------------------------------------
+* Project Name : SQL_memOJi
+* File Name    : tests.py
+* Description  : 
+* Create Time  : 2021-04-04 00:43:21
+* Version      : 1.0
+* Author       : Steve X
+* GitHub       : https://github.com/Steve-Xyh/SQL_memOJi
+----------------------------------------------------------------------------------------------------
+* Notice
+- 
+- 
+----------------------------------------------------------------------------------------------------
+'''
+
+
 from django.test import TestCase
 
 # Create your tests here.
