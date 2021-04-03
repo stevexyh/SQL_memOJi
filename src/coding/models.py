40fbf627ecc7790e607ceb509d8b1a64b08c272e
Steve X <stevexyh@hotmail.com>
2021-04-04 00:47:21 +0800

    chore(coding): added headers for python

--- src/coding/models.py
+++ src/coding/models.py
@@ -1,3 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'''
+----------------------------------------------------------------------------------------------------
+* Project Name : SQL_memOJi
+* File Name    : models.py
+* Description  : 
+* Create Time  : 2021-04-04 00:46:35
+* Version      : 1.0
+* Author       : Steve X
+* GitHub       : https://github.com/Steve-Xyh/SQL_memOJi
+----------------------------------------------------------------------------------------------------
+* Notice
+- 
+- 
+----------------------------------------------------------------------------------------------------
+'''
+
+
 from django.db import models
 
 # Create your models here.
