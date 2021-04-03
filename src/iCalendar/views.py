3a6b876cc632751f6efbea7f918eba43d55e46fd
Steve X <stevexyh@hotmail.com>
2021-04-04 00:48:33 +0800

    chore(iCalendar): added headers for python

--- src/iCalendar/views.py
+++ src/iCalendar/views.py
@@ -1,3 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'''
+----------------------------------------------------------------------------------------------------
+* Project Name : SQL_memOJi
+* File Name    : views.py
+* Description  : 
+* Create Time  : 2021-04-04 00:48:04
+* Version      : 1.0
+* Author       : Steve X
+* GitHub       : https://github.com/Steve-Xyh/SQL_memOJi
+----------------------------------------------------------------------------------------------------
+* Notice
+- 
+- 
+----------------------------------------------------------------------------------------------------
+'''
+
+
 from django.shortcuts import render
 
 # Create your views here.
