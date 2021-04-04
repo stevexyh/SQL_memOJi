9aff0a169e2478a1cbf83049f1b2497cad638e1c
Steve X <stevexyh@hotmail.com>
2021-04-04 20:24:11 +0800

    chore: removed useless code

--- src/SQL_memOJi/urls.py
+++ src/SQL_memOJi/urls.py
@@ -33,7 +33,6 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
-from django.views.static import serve
 import user.urls
 
 
--- src/user/urls.py
+++ src/user/urls.py
@@ -17,7 +17,7 @@
 ----------------------------------------------------------------------------------------------------
 '''
 
-from django.urls import path, include
+from django.urls import path
 from . import views
 
 app_name = 'user'
