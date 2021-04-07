54c5c441dc94c3f00504b913c92db15921a8330d
Steve X <stevexyh@hotmail.com>
2021-04-07 20:26:31 +0800

    feat(user): finished log in function

--- src/user/urls.py
+++ src/user/urls.py
@@ -5,15 +5,15 @@
 ----------------------------------------------------------------------------------------------------
 * Project Name : SQL_memOJi
 * File Name    : urls.py
-* Description  : 
+* Description  :
 * Create Time  : 2021-04-04 15:45:56
 * Version      : 1.0
 * Author       : Steve X
 * GitHub       : https://github.com/Steve-Xyh/SQL_memOJi
 ----------------------------------------------------------------------------------------------------
 * Notice
-- 
-- 
+-
+-
 ----------------------------------------------------------------------------------------------------
 '''
 
@@ -29,9 +29,10 @@ urlpatterns = [
 
     # Auth Pages
     path('auth-base/', views.auth_base, name='auth-base'),
-    path('auth-login/', views.auth_login, name='auth-login'),
+    path('auth-login/', views.AuthLogin.as_view(), name='auth-login'),
     path('auth-recoverpw/', views.auth_recoverpw, name='auth-recoverpw'),
     path('auth-register/', views.auth_register, name='auth-register'),
+    path('auth-status/', views.auth_status, name='auth-status'),
 
     # Management Pages
     path('class-manage/', views.class_manage, name='class-manage'),
--- src/user/views.py
+++ src/user/views.py
@@ -5,20 +5,23 @@
 ----------------------------------------------------------------------------------------------------
 * Project Name : SQL_memOJi
 * File Name    : views.py
-* Description  : 
+* Description  :
 * Create Time  : 2021-04-04 00:43:27
 * Version      : 1.0
 * Author       : Steve X
 * GitHub       : https://github.com/Steve-Xyh/SQL_memOJi
 ----------------------------------------------------------------------------------------------------
 * Notice
-- 
-- 
+-
+-
 ----------------------------------------------------------------------------------------------------
 '''
 
 
 from django.shortcuts import render
+from django.views import View
+from django.contrib import auth
+from django.utils.translation import gettext_lazy as _
 
 # Create your views here.
 
@@ -50,10 +53,35 @@ def auth_base(request):
     return render(request, 'user/auth-base.html')
 
 
-def auth_login(request):
-    '''Render auth-login template'''
+class AuthLogin(View):
+    '''Render auth-login template in CBV'''
 
-    return render(request, 'user/auth-login.html')
+    def get(self, request):
+        return render(request, 'user/auth-login.html')
+
+    def post(self, request):
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = auth.authenticate(username=username, password=password)
+        success = bool(user)
+        msg = _('登录成功') if success else _('用户名或密码错误')
+        content = {
+            'user': user,
+            'success': success,
+            'msg': msg,
+        }
+
+        if not success:
+            return render(request, 'user/auth-login.html', context=content)
+
+        auth.login(request, user=user)
+        return render(request, 'user/auth-status.html', context=content)
+
+
+def auth_status(request):
+    '''Render auth-status template'''
+
+    return render(request, 'user/auth-status.html')
 
 
 def auth_recoverpw(request):
