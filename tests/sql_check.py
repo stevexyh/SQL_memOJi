4d730cf7bec1bb7998416cbe91e09446b52551ab
Steve X <stevexyh@hotmail.com>
2021-05-06 16:23:53 +0800

    chore(tests): created test files for SQL

---
+++ tests/sql_check.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'''
+----------------------------------------------------------------------------------------------------
+* Project Name : SQL_memOJi
+* File Name    : sql_check.py
+* Description  : check the correctness for each SQL
+* Create Time  : 2021-05-06 16:22:23
+* Version      : 1.0
+* Author       : Steve X
+* GitHub       : https://github.com/Steve-Xyh/SQL_memOJi
+----------------------------------------------------------------------------------------------------
+* Notice
+- 
+- 
+----------------------------------------------------------------------------------------------------
+'''
