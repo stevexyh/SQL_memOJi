6a75ce738df86a0de99fc42f3f133b9805e4cae7
SeddonShen <283481855@qq.com>
2022-01-09 17:27:59 +0800

    calendar finish

--- src/iCalendar/views.py
+++ src/iCalendar/views.py
@@ -17,12 +17,18 @@
 ----------------------------------------------------------------------------------------------------
 '''
 
-
+import json
 from django.shortcuts import render
-
-# Create your views here.
+import datetime
 from user.models import Student, User, Classroom
 from coding.models import Exam,Exercise
+#XXX(Seddon):默认按照中国市区
+class DateEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj,datetime.datetime):
+            return obj.strftime("%Y-%m-%d %H:%M:%S")
+        else:
+            return json.JSONEncoder.default(self,obj)
 
 def calendar(request):
     '''Render calendar template'''
@@ -36,4 +42,9 @@ def calendar(request):
         'exams_list': exams_list,
         'exer_list': exer_list,
     }
-    return render(request, 'iCalendar/calendar.html')
+    calc_list = []
+    for exam in exams_list:
+        calc_list.append({'title':exam.exam_name,'start':exam.start_time + datetime.timedelta(hours=8),'end':exam.end_time + datetime.timedelta(hours=8)})
+    for exer in exer_list:
+        calc_list.append({'title':exer.exer_name,'start':exer.publish_time + datetime.timedelta(hours=8)})
+    return render(request, 'iCalendar/calendar.html', {'spots':json.dumps(calc_list,cls=DateEncoder)})
